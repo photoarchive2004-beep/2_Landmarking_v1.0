56b34bd787e1a1927a9bae6e5c3f3a5db0939fdf
photoarchive2004-beep <photoarchive2004@gmail.com>
2025-11-13 22:58:00 +0300

    diag: TK check + last EXIT + tails @20251113_225800

---
+++ logs/hello_tk_check_20251113_225800.py
@@ -0,0 +1,8 @@
+﻿import sys
+print('PY', sys.version)
+try:
+    import tkinter as tk, _tkinter
+    r=tk.Tk(); r.withdraw(); r.update_idletasks(); r.destroy()
+    print('TK HELLO: PASS')
+except Exception as e:
+    print('TK HELLO: FAIL', repr(e))
