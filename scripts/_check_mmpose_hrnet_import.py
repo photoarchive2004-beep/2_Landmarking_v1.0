a7c2308257e8f290957c1402588e5c25576bdd0a
photoarchive2004-beep <photoarchive2004@gmail.com>
2025-11-16 14:32:04 +0300

    env: rebuild xtcocotools for NumPy compatibility (MMPose HRNet)

---
+++ scripts/_check_mmpose_hrnet_import.py
@@ -0,0 +1,10 @@
+﻿import numpy, traceback
+
+print("=== MMPose HRNet import check v2 ===")
+print("numpy version:", numpy.__version__)
+try:
+    from mmpose.models.backbones import HRNet as MMPoseHRNet
+    print("MMPoseHRNet imported OK:", MMPoseHRNet)
+except Exception as e:
+    print("IMPORT_ERROR:", repr(e))
+    traceback.print_exc()
